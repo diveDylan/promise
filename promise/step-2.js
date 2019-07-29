9d9a603bd7a25abc1b75eb3ec41168ee34327d30
曾祥达 <zengxiangda@zhongan.io>
2019-07-30 00:49:26 +0800

    step 2 callback time

---
+++ promise/step-2.js
@@ -0,0 +1,51 @@
+'use strict'
+const PENDING = 'pending'
+const RESOLVE = 'resolve'
+const REJECT = 'reject'
+/**
+ * 实现一个promise类可以使用一次then方法
+ * @param {*} resolve 
+ * @param {*} reject 
+ */
+function BasePromise(fn) {
+  this.status = PENDING
+  this.value = this.error = null
+  this.resolveQueen = []
+  this.rejectQueen = []
+  const self = this
+  function resolve (val) {
+    if (self.status === PENDING) {
+      self.value = val
+      self.status = RESOLVE
+      self.resolveQueen.forEach(i => i(self.value))
+    }
+  }
+  function reject (val) {
+    if (self.status === PENDING) {
+      self.value = val
+      self.status = REJECT
+      self.rejectQueen.forEach(i => i())
+    }
+  }
+  fn(resolve, reject)
+  return this
+}
+
+
+BasePromise.prototype.then = function(res) {
+  const self = this
+  self.resolveQueen.push(res)
+}
+
+BasePromise.prototype.catch = function() {
+  console.error(this.error)
+}
+
+const a = new BasePromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success')
+    console.log('111')
+  }, 0);
+  console.log(222)
+})
+a.then((e) => console.log(e))
